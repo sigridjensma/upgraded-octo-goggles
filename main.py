65326609174aae0ef82764510774effed1ce0551
sigridjensma <s.jensma@ll.gymnasiumarnhem.nl>
2023-09-25 15:41:57 +0200

    verschillende commands voor het runnen van het spel toegevoegd, nog niet werkend gemaakt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Importing required modules
 import os
 import discord
+import random
+global repeat
 from dotenv import load_dotenv
 
 
@@ -25,10 +27,102 @@ async def on_ready():
     print("Jack Black is in " + str(guild_count) + " guilds.")
 
 
+def pickcard():
+    global deck
+    x = random.randint(0, (len(deck) - 1))
+    card = deck[x]
+    deck.pop(x)
+    return card
+
+
+def init():  # begin van het spel
+    global deck  # zo kan je geen dubbele kaarten trekken
+    deck = [2, 2, 2, 2,
+            3, 3, 3, 3,
+            4, 4, 4, 4,
+            5, 5, 5, 5,
+            6, 6, 6, 6,
+            7, 7, 7, 7,
+            8, 8, 8, 8,
+            9, 9, 9, 9,
+            10, 10, 10, 10,  # de 10
+            10, 10, 10, 10,  # de boer
+            10, 10, 10, 10,  # de vrouw
+            10, 10, 10, 10,  # de koning
+            11, 11, 11, 11]  # de aas
+    # global N
+    global total  # het totaal van users kaarten
+    global compkaarten  # het totaal van de computers kaarten
+    global ending # het einde van het spel
+    ending = False
+    # N = 1
+    message.channel.send("De computer trekt eerst een kaart")
+    kaart1computer = pickcard()
+    kaart2computer = pickcard()
+    compkaarten = [kaart1computer, kaart2computer]
+    message.channel.send("De eerste kaart van de computer is:", kaart1computer
+    "/n De tweede kaart van de computer krijg je aan het einde van het spel te zien")
+    message.channel.send("Nu mag jij 2 kaarten trekken.")
+    kaart1speler = pickcard()
+    kaart2speler = pickcard()
+    total = kaart1speler + kaart2speler
+    message.channel.send("Je hebt een", kaart1speler, "getrokken, en je hebt een", kaart2speler,
+                         "getrokken. \n Je totaal is nu", total)
+    if total >= 21:
+        ending = True
+    else:
+        ending = True
+
+
+def round():
+    global total
+    global ending
+    message.channel.send("Wil jij een nog een kaart?")
+    message2 = message.content.lower()
+    inputcorrection = False
+    while inputcorrection == False:
+        message.channel.send("Wil jij een nog een kaart?")
+        message2 = message.content.lower()
+        if message2 == "ja":
+            card = pickcard()
+            total = total + card
+            message.channel.send("Jij hebt getrokken", card, "/n Je totaal is nu", total)
+            inputcorrection = True
+        elif message2 == "nee":
+            message.channel.send("Oke.")
+            inputcorrection = True
+        else:
+            message.channel.send("Beantwoord met ja of nee")
+    if total >= 21:
+        ending = True
+    else:
+        ending = False
+
+
+
 @client.event   # start van het spel, op het moment dat de user wil spelen
 async def on_message(message):
-    if message.content.find("/play") != -1:
+    if message.content.startswith("/play") != -1:
+        global spelactief
+        if spelactief == True:
+            return
         await message.channel.send("oke, je wil met mij blackjacken")
+        await message.channel.send("Weet je hoe blackjacken werkt?")
+        message = message.content.lower() # ook als je geen hoofdletters gebruikt
+        if message == "nee":
+            spelactief = False # hier moet nog een invoercorrection over heen
+        elif message =="ja":
+            spelactief = True
+        else:
+            message.channel.send("graag 'ja' of 'nee'")
+
+
+
+
+
+
+
+
 
 
 load_dotenv()
